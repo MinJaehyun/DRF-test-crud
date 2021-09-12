dadc287f3fa4c2e5b0a4c934c1f8b3f0b11c8938
{krism01} <{krism01@naver.com}>
2021-09-12 13:40:30 +0900

    Add articleapp

--- articleapp/views.py
+++ articleapp/views.py
@@ -7,3 +7,6 @@ from articleapp.serializers import ArticleSerializer
 class ArticleViewSet(ModelViewSet):
   queryset         = Article.objects.all()
   serializer_class = ArticleSerializer
+
+  def perform_create(self, serializer):
+    serializer.save(user=self.request.user)
