bdde2dad6b360943856c7bcbfc706a96d469956c
{krism01} <{krism01@naver.com}>
2021-09-12 23:40:42 +0900

    게시글을 작성한 유저만 변경할 수 있도록 권한 설정

--- articleapp/views.py
+++ articleapp/views.py
@@ -1,12 +1,18 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework import viewsets
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 
-from articleapp.models import Article
-from articleapp.serializers import ArticleSerializer
+from .models import Article
+from .permissions import IsOwnerOrReadOnly
+from .serializers import ArticleSerializer
 
 
-class ArticleViewSet(ModelViewSet):
-  queryset         = Article.objects.all()
-  serializer_class = ArticleSerializer
+# Blog의 list, create, retrieve, update, destroy
+class ArticleViewSet(viewsets.ModelViewSet):
+  authentication_classes = [BasicAuthentication, SessionAuthentication]    # authentication 추가
+  permission_classes     = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]  # permission 추가
+  queryset               = Article.objects.all()
+  serializer_class       = ArticleSerializer
 
   def perform_create(self, serializer):
     serializer.save(user=self.request.user)
